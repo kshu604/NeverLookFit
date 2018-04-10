e24cfa8424ed3f2021b4207f8ef9b40f85d6a77e
dan <danretonel@gmail.com>
2018-04-09 19:19:08 -0700

    Exercise class
    Signed-off-by: dan <danretonel@gmail.com>

---
+++ app/src/main/java/com/example/neverlookfit/activities/RecordExercise.java
@@ -0,0 +1,15 @@
+package com.example.neverlookfit.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.neverlookfit.R;
+
+public class RecordExercise extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record_exercise);
+    }
+}
